8581e80353bad74c04dd6795fc36337e5f4ea83f
nanosite0 <lmonto@proton.me>
2026-04-15 18:13:40 -0500

    maps api test
    testing google maps api in javascript and html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function init(){
+  
+  var mapping = {
+    center:new google.maps.LatLng(38.7946,-106.5348),
+    zoom:5,
+  };
+  
+  //var size = {
+  //  width:100%,
+  //  height:400px,
+  //};
+  
+  var map = new google.maps.Map(mapping);
+  
+  
+  
+}
